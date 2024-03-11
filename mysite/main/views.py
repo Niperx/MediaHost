c19753f17ae51b6fae5793d3fbe17a5cd8e44792
Daniel <79987802+Niperx@users.noreply.github.com>
2024-03-11 22:37:42 +1100

    init

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -39,6 +39,9 @@ class ProfileView(TemplateView):
         if self.request.user.is_authenticated:
             username = self.request.user
         context = super().get_context_data(**kwargs)
+        # username = self.kwargs.get("username")
+        # print(self.kwargs.get("username"))
+        print(username)
         context['photos'] = Photo.objects.all().filter(created_by=username).order_by("-created_at")
         return context
 
